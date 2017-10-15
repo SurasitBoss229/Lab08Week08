4b6eef55c5d3773ac374b467aa07371e7f2fe377
Surasit <surasitboss229@gmail.com>
2017-10-15 17:50:28 +0700

    WP08P03

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,15 +7,26 @@ namespace ConsoleAppArray
         static void Main(string[] args)
         {
             int[] data = new int[10];
-            data[-1] = 0;
-            data[1] = 1;
-            data[9] = 9;
-            data[10] = 10;
-            Console.WriteLine(data[-1]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[9]);
-            Console.WriteLine(data[10]);
+            data[0] = 0; data[1] = 1; data[2] = 2; data[3] = 3; data[4] = 4;
+            data[5] = 5; data[6] = 6; data[7] = 7; data[8] = 8; data[9] = 9;
+            Console.WriteLine(data[7]);
+            Console.WriteLine(data[4] + data[9]);
+            if (data[2] == data[6])
+            {
+                Console.WriteLine("data[2] = data[6]");
+            }
+            else
+            {
+                Console.WriteLine("data[2] != data[6]");
+            }
+            Console.WriteLine("Sum : {0}", data [0] + data[1] + data[2] + data[3] 
+                + data[4] + data[5] + data[6] + data[7] + data[8] + data[9]);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write(data[i] + " ");
+            }
             Console.ReadLine();
+
         }
     }
 }
